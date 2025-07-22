0b4342ac45b854dcc694cf7ba4de6fce8b98419e
Francis Vino <flvthirty@gmail.com>
2025-07-23 02:28:52 +0800

    refator: dark mode button code + style

--- scripts/darkMode.js
+++ scripts/darkMode.js
@@ -5,12 +5,10 @@ document.addEventListener("DOMContentLoaded", () => {
   const savedMode = localStorage.getItem("dark-mode");
   if (savedMode === "true") {
     document.body.classList.add("dark-mode");
-    toggle.textContent = "☀️";
   }
 
   toggle.addEventListener("click", () => {
     const isDark = document.body.classList.toggle("dark-mode");
-    toggle.textContent = isDark ? "☀️" : "🌙";
     localStorage.setItem("dark-mode", isDark);
   });
 });
